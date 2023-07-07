311c98e4fe9f9e6003fdfef38e87692207538874
phucgari <phucgari@gmail.com>
2023-07-07 21:05:12 +0700

    fix countToken

--- countToken.py
+++ countToken.py
@@ -5,17 +5,87 @@ load_dotenv()
 
 model_name=os.getenv("MODEL_NAME")
 
-def num_tokens_from_string(string: str, encoding_name: str) -> int:
-    encoding = tiktoken.encoding_for_model(encoding_name)
-    num_tokens = len(encoding.encode(string))
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
+    """Return the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model in {
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-16k-0613",
+        "gpt-4-0314",
+        "gpt-4-32k-0314",
+        "gpt-4-0613",
+        "gpt-4-32k-0613",
+        }:
+        tokens_per_message = 3
+        tokens_per_name = 1
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif "gpt-3.5-turbo" in model:
+        print("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
+    elif "gpt-4" in model:
+        print("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
+        return num_tokens_from_messages(messages, model="gpt-4-0613")
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+        )
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
     return num_tokens
 
-def num_tokens_from_messages(messages)->int:
-    sum=0
-    for message in messages:
-        sum+=num_tokens_from_string(message["content"],model_name)
-    return sum
+def num_tokens_from_functions(functions, model="gpt-3.5-turbo-0613"):
+        """Return the number of tokens used by a list of functions."""
+        try:
+            encoding = tiktoken.encoding_for_model(model)
+        except KeyError:
+            print("Warning: model not found. Using cl100k_base encoding.")
+            encoding = tiktoken.get_encoding("cl100k_base")
+        
+        num_tokens = 0
+        for function in functions:
+            function_tokens = len(encoding.encode(function['name']))
+            function_tokens += len(encoding.encode(function['description']))
+            
+            if 'parameters' in function:
+                parameters = function['parameters']
+                if 'properties' in parameters:
+                    for propertiesKey in parameters['properties']:
+                        function_tokens += len(encoding.encode(propertiesKey))
+                        v = parameters['properties'][propertiesKey]
+                        for field in v:
+                            if field == 'type':
+                                function_tokens += 2
+                                function_tokens += len(encoding.encode(v['type']))
+                            elif field == 'description':
+                                function_tokens += 2
+                                function_tokens += len(encoding.encode(v['description']))
+                            elif field == 'enum':
+                                function_tokens -= 3
+                                for o in v['enum']:
+                                    function_tokens += 3
+                                    function_tokens += len(encoding.encode(o))
+                            else:
+                                print(f"Warning: not supported field {field}")
+                    function_tokens += 11
 
-def truncate_messages(messages):
-    while(num_tokens_from_messages(messages)>=4000):
+            num_tokens += function_tokens
+
+        num_tokens += 12 
+        return num_tokens
+
+def truncate_messages(messages, functions):
+    tokens_in_functions=num_tokens_from_functions(functions)
+    while(num_tokens_from_messages(messages)+tokens_in_functions>=4000):
         messages.pop(1)
--- test.py
+++ test.py
@@ -8,6 +8,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 temperature = 1
 cv_detail = open("cv_detail.txt", encoding='UTF-8').readlines()
 messages = []
+functions = []
 
 messages.append({"role": "system", "content": f"""
 You are an assistant named Poppy which role is to introduce about a person have this CV which is in the delimiter bellow:{cv_detail}.
@@ -20,7 +21,7 @@ while True:
     messages.append({"role": "user", "content":
                     f"""Using the CV to reply to the requested message in delimiter. If there are not related information in the CV, reply: "Sorry this is not under my knowledge".
                     requested message: "{message}" """})
-    truncate_messages(messages)
+    truncate_messages(messages,functions)
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         temperature=temperature,
